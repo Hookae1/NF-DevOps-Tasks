a8bdf4661cab6e56d8c9b0d95d0b4f6df2d460c5
Hookae1 <rybitskyiy@gmail.com>
2022-09-14 14:10:25 +0000

    custom scripr v12

--- Terraform/Customscrpit/runner.sh
+++ Terraform/Customscrpit/runner.sh
@@ -19,7 +19,7 @@ sudo chmod -R 755 /var/lib/waagent
 sudo chmod -R 777 /var/lib/waagent/custom-script/download/0/actions-runner
 
 # Create the runner and start the configuration experience
-printf "Default\nmoonserver\n" | RUNNER_ALLOW_RUNASROOT="1" ./config.sh --url https://github.com/Hookae1/NF-DevOps-Tasks --token AQER23RYKFLUIPLCFMYCGJ3DEHT6A 
+printf "Default\nmoonserver\n" | RUNNER_ALLOW_RUNASROOT="1" ./config.sh --url https://github.com/Hookae1/NF-DevOps-Tasks --token AQER23VUFFA6KZKHMLG72UTDEHXL4 
 
 # Last step, run it!
 RUNNER_ALLOW_RUNASROOT="1" ./run.sh

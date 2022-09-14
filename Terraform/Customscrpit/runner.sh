16ed2a2a0ac5343eb7efd2f0d271310b147140f3
Hookae1 <rybitskyiy@gmail.com>
2022-09-14 11:08:25 +0000

    custom script v6

--- Terraform/Customscrpit/runner.sh
+++ Terraform/Customscrpit/runner.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+sudo su moonuser
+
 # Create a folder
 mkdir actions-runner && cd actions-runner
 
@@ -13,7 +17,6 @@ tar xzf ./actions-runner-linux-x64-2.296.2.tar.gz
 # Change permissions and ownership to a folder
 sudo chmod -R 755 /var/lib/waagent
 sudo chmod -R 777 /var/lib/waagent/custom-script/download/0/actions-runner
-sudo su moonuser
 
 # Create the runner and start the configuration experience
 printf "Default\nmoonserver\n" | ./config.sh --url https://github.com/Hookae1/NF-DevOps-Tasks --token AQER23R473PJZ3ZLOE5SLF3DEGYNC 

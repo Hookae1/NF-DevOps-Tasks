a76fc2d3b7089a7e7a6a4babfc6d80c22e5b6526
Hookae1 <azureuser@Server1.3zsku4fnp1uu3eligfa203sh5e.ax.internal.cloudapp.net>
2022-09-14 09:18:16 +0000

    custom script v1

--- Terraform/Customscrpit/runner.sh
+++ Terraform/Customscrpit/runner.sh
@@ -1,5 +1,5 @@
 # Create a folder
-mkdir actions-runner && cd actions-runner
+mkdir /home/$USER/actions-runner && cd /home/$USER/actions-runner
 
 # Download the latest runner package
 curl -o actions-runner-linux-x64-2.296.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.2/actions-runner-linux-x64-2.296.2.tar.gz
@@ -9,6 +9,10 @@ echo "34a8f34956cdacd2156d4c658cce8dd54c5aef316a16bbbc95eb3ca4fd76429a  actions-
 
 # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.296.2.tar.gz
+
+# Change ownership to a folder
+
+sudo chown -R $USER:$USER /home/$USER/actions-runner
 
 # Create the runner and start the configuration experience
 ./config.sh --url https://github.com/Hookae1/NF-DevOps-Tasks --token AQER23R254TQIIAQMGINTSDDEDNIQ
